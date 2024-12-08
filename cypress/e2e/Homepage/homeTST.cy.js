c28e508ba7e7b8630c1e32a7c61ef7df587aa56c
Francisco Conte <francisco@duxsoftware.com.ar>
2024-12-08 16:38:52 -0300

    primer TST

---
+++ cypress/e2e/Homepage/homeTST.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress"/>
+
+describe('Homepage', { tags: '@homepage' }, () => {
+    
+
+
+    // beforeEach(() => {
+        
+    // })
+
+    // afterEach(() => {
+        
+    // })
+
+    it('First test', { tags: '@001' }, () => {
+        cy.visit('https://demoqa.com/')
+
+    })
+
+    
+})
