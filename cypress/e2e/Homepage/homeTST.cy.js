d365beed60e5735709b3edf37c3c1f189cdcf594
Francisco Conte <francisco@duxsoftware.com.ar>
2025-04-02 14:53:25 -0300

    mocha

--- cypress/e2e/Homepage/homeTST.cy.js
+++ cypress/e2e/Homepage/homeTST.cy.js
@@ -1,4 +1,4 @@
-<reference types="cypress"/>
+
 
 describe('Pagina principal', { tags: '@homepage' }, () => {
     
@@ -14,9 +14,16 @@ describe('Pagina principal', { tags: '@homepage' }, () => {
 
     it('First test', { tags: '@001' }, () => {
         cy.visit('https://demoqa.com/')
-        
+        cy.url('https://demoqa.com/').should('be.eq', 'https://demoqa.com/')
         
     })
+
+    it('Second test', { tags: '@002' }, () => {
+        cy.visit('https://www.comocriarmariposas.com.ar/')
+        cy.url('https://www.comocriarmariposas.com.ar/').should('not.be.eq', 'https://demoqa.com/')
+        
+    })
+
 
     
 })
